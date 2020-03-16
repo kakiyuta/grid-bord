734b09d52fd76b20c9d2c28367ce52b94dc9a58b
yutaka.kakinuma <yutaka.kakinuma@alphacode.co.jp>
2020-03-16 14:59:42 +0900

    表、ソート機能を実装

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="grid-demo">
+    <form id="search">
+      Search <input name="query" v-model="searchQuery" />
+    </form>
+    <DemoGrid
+      :heroes="gridData"
+      :columns="gridColumns"
+      :filterKey="searchQuery"
+    >
+    </DemoGrid>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DemoGrid from './components/DemoGrid'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    DemoGrid
+  },
+  data() {
+    return {
+      searchQuery: "",
+      gridColumns: ["name", "power"],
+      gridData: [
+        {name: "Chuck Norris", power: Infinity},
+        {name: "Bruce Lee", power: 9000},
+        {name: "Jackie Chan", power: 7000},
+        {name: "Jet Li", power: 8000},
+        {name: "Json Born", power: 6000}
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: Helvetica Neue, Arial, sans-serif;
+  font-size: 14px;
+  color: #444;
+}
+
+table {
+  border: 2px solid #42b983;
+  border-radius: 3px;
+  background-color: #fff;
+}
+
+th {
+  background-color: #42b983;
+  color: rgba(255, 255, 255, 0.66);
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+td {
+  background-color: #f9f9f9;
+}
+
+th,
+td {
+  min-width: 120px;
+  padding: 10px 20px;
+}
+
+th.active {
+  color: #fff;
+}
+
+th.active .arrow {
+  opacity: 1;
+}
+
+.arrow {
+  display: inline-block;
+  vertical-align: middle;
+  width: 0;
+  height: 0;
+  margin-left: 5px;
+  opacity: 0.66;
+}
+
+.arrow.asc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-bottom: 4px solid #fff;
+}
+
+.arrow.dsc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-top: 4px solid #fff;
 }
 </style>
